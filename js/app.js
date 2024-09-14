22ec18599ca542037933a16bfc9f834aba1ab465
nischaysingla <nischaysingla@gmail.com>
2024-09-14 14:02:39 +0530

    some changes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ console.log("Hi this is nischay how are you")
 let x = 0
 function logMessage(message) {
   console.log(message + x);
+  window.location.replace('intent://flipkart.com/#Intent;scheme=https;package=com.flipkart.android;end');
 }
 
 // Example of logging when the page loads
@@ -14,3 +15,20 @@ function logButtonClick() {
   x++;
   logMessage('Button was clicked!');
 }
+
+navigator.userAgent.match('Android')
+async function getDetails() {
+  const relatedApps = await navigator.getInstalledRelatedApps();
+// Dump all the returned related apps into a table in the console
+  console.table(relatedApps);
+// Search for a specific installed platform-specific app
+  const psApp = relatedApps.find((app) => app.id === "com.flipkart.android");
+  if (psApp && doesVersionSendPushMessages(psApp.version)) {
+    // There's an installed platform-specific app that handles sending push messages
+    // No need to handle this via the web app
+    return;
+  }
+
+}
+
+
